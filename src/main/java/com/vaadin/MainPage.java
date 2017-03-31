5b783019774c3dd1fc4cd31b16bacb686fdcb7a8
Diego Pinheiro Cardoso <diegocardoso@g-10-64-12-147.guest.vaadin.com>
2017-03-31 16:52:31 +0300

    Create main layout for Bugrap

---
+++ src/main/java/com/vaadin/MainPage.java
@@ -0,0 +1,43 @@
+package com.vaadin;
+
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.CheckBoxGroup;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import org.vaadin.hene.popupbutton.PopupButton;
+
+/**
+ * Created by diegocardoso on 3/31/17.
+ */
+public class MainPage extends MainPageDesign {
+    private final MyUI myUI;
+    private final PopupButton customStatusPopupBtn;
+
+    public MainPage(MyUI myUI) {
+        this.myUI = myUI;
+
+        logoutLink.setIcon(VaadinIcons.KEY);
+        reportBugLink.setIcon(VaadinIcons.BUG);
+        requestFeatureLink.setIcon(VaadinIcons.LIGHTBULB);
+        manageProjectLink.setIcon(VaadinIcons.COG);
+
+        searchIconLabel.setIcon(VaadinIcons.SEARCH);
+        searchIconLabel.setCaption("");
+
+        searchCloseLabel.setIcon(VaadinIcons.CLOSE);
+        searchCloseLabel.setCaption("");
+
+        customStatusPopupBtn = new PopupButton("Custom");
+        VerticalLayout customStatusLayout = new VerticalLayout();
+        customStatusPopupBtn.setContent(customStatusLayout);
+
+        Label customStatusTitle = new Label("STATUS");
+        customStatusLayout.addComponent(customStatusTitle);
+
+        CheckBoxGroup customStatusOptions = new CheckBoxGroup();
+        customStatusOptions.setItems("Open", "Fixed", "Invalid", "Won't fix", "Can't fix", "Duplicate", "Works for me", "Needs more information");
+        customStatusLayout.addComponent(customStatusOptions);
+
+        statusOptions.addComponent(customStatusPopupBtn);
+    }
+}
