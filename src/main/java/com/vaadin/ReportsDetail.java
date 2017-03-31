fd9e95d47b7c46ffe2094131130b027b101c6053
Diego Pinheiro Cardoso <diegocardoso@g-10-64-12-147.guest.vaadin.com>
2017-03-31 16:53:24 +0300

    Create layout for report details/editing

---
+++ src/main/java/com/vaadin/ReportsDetail.java
@@ -0,0 +1,14 @@
+package com.vaadin;
+
+/**
+ * Created by diegocardoso on 3/31/17.
+ */
+public class ReportsDetail extends ReportsDetailDesign {
+
+    private MyUI myUI;
+
+    public ReportsDetail(MyUI myUI) {
+        this.myUI = myUI;
+    }
+
+}
