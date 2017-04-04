757a5e004f93c060a307cfcfcc3125f718b71af6
Diego Pinheiro Cardoso <diegocardoso@g-10-64-12-147.guest.vaadin.com>
2017-04-04 16:53:49 +0300

    Add more bindings and behavior to the elements on MainPage

--- src/main/java/com/vaadin/MainPage.java
+++ src/main/java/com/vaadin/MainPage.java
@@ -4,10 +4,16 @@ import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.CheckBoxGroup;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.renderers.DateRenderer;
 import org.vaadin.bugrap.domain.entities.Project;
+import org.vaadin.bugrap.domain.entities.ProjectVersion;
+import org.vaadin.bugrap.domain.entities.Report;
+import org.vaadin.bugrap.domain.entities.Reporter;
 import org.vaadin.hene.popupbutton.PopupButton;
 
-import java.util.stream.Stream;
+import java.text.DateFormat;
+import java.util.*;
 
 /**
  * Created by diegocardoso on 3/31/17.
@@ -15,14 +21,25 @@ import java.util.stream.Stream;
 public class MainPage extends MainPageDesign {
     private final MyUI myUI;
     private final PopupButton customStatusPopupBtn;
+    private final Grid<Report> reportGrid = new Grid<>(Report.class);
+    private final Reporter reporter;
+    private final CheckBoxGroup<Report.Status> customStatusOptions;
 
-    public MainPage(MyUI myUI) {
+    private Project selectedProject;
+    private ProjectVersion selectedProjectVersion;
+    private Reporter selectedAssignee;
+    private Set<Report.Status> selectedStatuses;
+
+    public MainPage(MyUI myUI, Reporter reporter) {
         this.myUI = myUI;
+        this.reporter = reporter;
 
         logoutLink.setIcon(VaadinIcons.KEY);
         reportBugLink.setIcon(VaadinIcons.BUG);
         requestFeatureLink.setIcon(VaadinIcons.LIGHTBULB);
         manageProjectLink.setIcon(VaadinIcons.COG);
+
+        accountNameLabel.setCaption(reporter.getName());
 
         searchIconLabel.setIcon(VaadinIcons.SEARCH);
         searchIconLabel.setCaption("");
@@ -37,18 +54,125 @@ public class MainPage extends MainPageDesign {
         Label customStatusTitle = new Label("STATUS");
         customStatusLayout.addComponent(customStatusTitle);
 
-        CheckBoxGroup customStatusOptions = new CheckBoxGroup();
-        customStatusOptions.setItems("Open", "Fixed", "Invalid", "Won't fix", "Can't fix", "Duplicate", "Works for me", "Needs more information");
+        customStatusOptions = new CheckBoxGroup();
+        customStatusOptions.setItems(Report.Status.values());
+        customStatusOptions.select(Report.Status.values());
         customStatusLayout.addComponent(customStatusOptions);
+        selectedStatuses = customStatusOptions.getSelectedItems();
 
         statusOptions.addComponent(customStatusPopupBtn);
 
-        Stream<String> streamOfItems = myUI.getProjects().stream().map(project -> project.getName());
-        Project firstProject = myUI.getProjects().iterator().next();
+        List<Project> projectsList = new ArrayList<>(myUI.getProjects());
+        Project firstProject = projectsList.get(0);
 
-        projectSelectorCombo.setItems(streamOfItems);
-        projectSelectorCombo.setSelectedItem(firstProject.getName());
+        projectSelectorCombo.setItems(projectsList);
+        projectSelectorCombo.setSelectedItem(firstProject);
 
-        reportsTable.setItems(myUI.getReportsByProject(firstProject));
+        reportGrid.setSizeFull();
+
+        reportGrid.setColumns("priority", "type", "summary", "assigned");
+        reportGrid.getColumn("assigned").setCaption("Assigned to").setDescriptionGenerator(e -> e.getAssigned() != null? e.getAssigned().getName(): "");
+
+        DateFormat dateTimeFormatter = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT, Locale.ENGLISH);
+        reportGrid.addColumn(Report::getTimestamp).setCaption("Last modified").setRenderer(new DateRenderer(dateTimeFormatter));
+        reportGrid.addColumn(Report::getReportedTimestamp).setCaption("Reported").setRenderer(new DateRenderer(dateTimeFormatter));
+
+        tableDetailsSession.addComponent(reportGrid);
+
+        loadProject(firstProject);
+
+        projectSelectorCombo.addValueChangeListener(e -> onChangeProject());
+        projectVersionsCombo.addValueChangeListener(e -> onChangeProjectVersion());
+
+        onlyMeAssigneeBtn.addClickListener(e -> onClickOnlyMeBtn());
+        everyoneAssigneeBtn.addClickListener(e -> onClickEveryoneBtn());
+
+        openStatusBtn.addClickListener(e -> onClickOpenStatusBtn());
+        allKindsStatusBtn.addClickListener(e -> onClickAllKindsStatusBtn());
+        customStatusOptions.addSelectionListener(e -> onSelectCustomStatusOptions());
+    }
+
+    private void loadProject (Project project) {
+        Set<ProjectVersion> versionsByProject = myUI.getVersionsByProject(project);
+        List<ProjectVersion> versionByProjectList = new ArrayList<>(versionsByProject);
+
+        if  (versionsByProject.size() > 1) {
+            ProjectVersion allVersions = new ProjectVersion();
+            allVersions.setProject(project);
+            allVersions.setVersion("All versions");
+
+            versionByProjectList.add(0, allVersions);
+        }
+
+        ProjectVersion firstVersion = versionByProjectList.get(0);
+        projectVersionsCombo.setItems(versionByProjectList);
+        projectVersionsCombo.setSelectedItem(firstVersion);
+
+        selectedProject = project;
+        selectedProjectVersion = firstVersion;
+
+        reportGrid.setItems(myUI.filterReportsByProject(selectedProject, selectedProjectVersion, selectedStatuses));
+    }
+
+    private void onChangeProjectVersion() {
+        selectedProjectVersion = projectVersionsCombo.getSelectedItem().get();
+        selectedProject = selectedProjectVersion.getProject();
+
+        reportGrid.setItems(myUI.getReportsByProject(selectedProject, selectedProjectVersion));
+    }
+
+    private void onChangeProject() {
+        Optional<Project> selectedItem = projectSelectorCombo.getSelectedItem();
+        Project selectedProject = selectedItem.get();
+
+
+        loadProject(selectedProject);
+    }
+
+    private void onClickOnlyMeBtn() {
+        onlyMeAssigneeBtn.addStyleName("selected");
+        everyoneAssigneeBtn.removeStyleName("selected");
+
+        selectedProjectVersion = projectVersionsCombo.getSelectedItem().get();
+        selectedProject = selectedProjectVersion.getProject();
+        selectedStatuses = customStatusOptions.getSelectedItems();
+        selectedAssignee = reporter;
+
+        reportGrid.setItems(myUI.filterReportsByProject(selectedProject, selectedProjectVersion, selectedStatuses, selectedAssignee));
+    }
+
+    private void onClickEveryoneBtn() {
+        everyoneAssigneeBtn.addStyleName("selected");
+        onlyMeAssigneeBtn.removeStyleName("selected");
+
+        selectedProjectVersion = projectVersionsCombo.getSelectedItem().get();
+        selectedProject = selectedProjectVersion.getProject();
+        selectedStatuses = customStatusOptions.getSelectedItems();
+        selectedAssignee = null;
+
+        reportGrid.setItems(myUI.filterReportsByProject(selectedProject, selectedProjectVersion, selectedStatuses));
+    }
+
+    private void onSelectCustomStatusOptions() {
+        selectedStatuses = customStatusOptions.getSelectedItems();
+
+        reportGrid.setItems(myUI.filterReportsByProject(selectedProject, selectedProjectVersion, selectedStatuses, selectedAssignee));
+    }
+
+    private void onClickAllKindsStatusBtn() {
+        customStatusOptions.select(Report.Status.values());
+
+        selectedStatuses = customStatusOptions.getSelectedItems();
+
+        reportGrid.setItems(myUI.filterReportsByProject(selectedProject, selectedProjectVersion, selectedStatuses, selectedAssignee));
+    }
+
+    private void onClickOpenStatusBtn() {
+        customStatusOptions.deselectAll();
+        customStatusOptions.select(Report.Status.OPEN);
+
+        selectedStatuses = customStatusOptions.getSelectedItems();
+
+        reportGrid.setItems(myUI.filterReportsByProject(selectedProject, selectedProjectVersion, selectedStatuses, selectedAssignee));
     }
 }
